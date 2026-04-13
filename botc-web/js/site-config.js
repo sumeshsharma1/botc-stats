1dd00687f27c897b109e6aa09164ee21fb74755b
Sumesh Sharma <35505694+sumeshsharma1@users.noreply.github.com>
2026-04-12 22:50:50 -0700

    Update site-config.js

--- botc-web/js/site-config.js
+++ botc-web/js/site-config.js
@@ -23,7 +23,7 @@ const SITE_CONFIG = {
     communityName: 'Sumesh & Friends Blood on the Clocktower :)',
 
     // Minimum games a player needs to appear on the leaderboard
-    minGamesForLeaderboard: 1,
+    minGamesForLeaderboard: 2,
 
     // ELO settings
     defaultRating: 1500,    // Starting ELO for new players
